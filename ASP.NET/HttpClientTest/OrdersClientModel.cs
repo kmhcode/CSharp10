cfb231d02f75fe3fac158a2ba0e8cab6f4f5f0dc
kmhcode <dacfaculty@localhost.localdomain>
2021-08-18 15:56:22 +0530

    Code

--- ASP.NET/HttpClientTest/OrdersClientModel.cs
+++ ASP.NET/HttpClientTest/OrdersClientModel.cs
@@ -1,42 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Net.Http;
+namespace HttpClientTest;
+
 using System.Net.Http.Json;
 
-namespace HttpClientTest
+public class Order
 {
-	public class Order
+	public int Id { get; set; }
+
+	public DateTime OrderDate { get; set; }
+
+	public string CustomerId { get; set; }
+
+	public int ProductNo { get; set; }
+
+	public int Quantity { get; set; }
+}
+
+public class OrdersClientModel
+{
+	const string serviceBase = "http://localhost:5000/api";
+
+	readonly HttpClient client = new();
+
+	public Task<List<Order>> GetCustomerOrdersAsync(string customerId)
 	{
-		public int Id { get; set; }
-
-		public DateTime OrderDate { get; set; }
-
-		public string CustomerId { get; set; }
-
-		public int ProductNo { get; set; }
-
-		public int Quantity { get; set; }
+		return client.GetFromJsonAsync<List<Order>>($"{serviceBase}/orders/{customerId}");
 	}
 
-	public class OrdersClientModel
+	public async Task<int> PostOrderAsync(Order entry)
 	{
-		const string serviceBase = "http://localhost:5000/api";
-
-		readonly HttpClient client = new();
-
-		public Task<List<Order>> GetCustomerOrdersAsync(string customerId)
-		{
-			return client.GetFromJsonAsync<List<Order>>($"{serviceBase}/orders/{customerId}");
-		}
-
-		public async Task<int> PostOrderAsync(Order entry)
-		{
-			var response = await client.PostAsJsonAsync($"{serviceBase}/orders", entry);
-			response.EnsureSuccessStatusCode();
-			var order = await response.Content.ReadFromJsonAsync<Order>();
-			return order.Id;
-		}
+		var response = await client.PostAsJsonAsync($"{serviceBase}/orders", entry);
+		response.EnsureSuccessStatusCode();
+		var order = await response.Content.ReadFromJsonAsync<Order>();
+		return order.Id;
 	}
 }
+
 
